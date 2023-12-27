3f1d339f9f5df89ef34998e8be493f1c9f5c2971
Karthik Kundurthy <55899975+Karthik-Kundurthy@users.noreply.github.com>
2023-12-27 14:54:11 -0500

    Time: 8 ms (21.15%), Space: 14.1 MB (16.87%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,6 +1,53 @@
+// practicing binary search and recursion in C++
 class Solution {
 public:
+    
+    //binary search solution
     vector<int> searchRange(vector<int>& nums, int target) {
+        int idx = binarySearch(nums, target, 0, nums.size() - 1);
+        std::cout << idx << std::endl;
+        vector<int> ret;
+        ret.push_back(-1);
+        ret.push_back(-1);
+        if (idx == -1) {
+            return ret;
+        } 
+        
+        int i = idx;
+        int j = idx;
+        while (i >= 0 && nums[i] == target) {
+            ret[0] = i;
+            i--;
+        }
+        
+        while (j < nums.size() && nums[j] == target) {
+            ret[1] = j;
+            j++;
+        }
+        
+        return ret;
+        
+        
+    }
+    
+    int binarySearch(vector<int>& nums, int target, int low, int high) {
+        // avoid infinite loop
+        if (low <= high) {
+            int mid = (low + high)/2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                return binarySearch(nums, target, mid + 1, high);
+            } else if (nums[mid] > target) {
+                return binarySearch(nums, target, low, mid - 1);
+            }  
+        }
+        
+        return -1;
+    } 
+    
+    // initially gave linear solution
+    vector<int> linearSearchRange(vector<int>& nums, int target) {
         vector<int> ret;
         ret.push_back(-1);
         ret.push_back(-1);
